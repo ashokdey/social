05bb815d3538af7a9bbde72320f77c2276aa0eb2
Ashok Dey <ashokdey100@gmail.com>
2017-08-14 22:55:23 +0530

    added compound index

---
+++ app/models/Followers.js
@@ -0,0 +1,32 @@
+'use strict';
+
+import mongoose from  'mongoose';
+
+const followersSchema = new mongoose.Schema({
+  followee: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Users',
+    required: true
+  },
+  follower: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Questions',
+    required:true,
+    unique: true
+  },
+  start: {
+    type: Date,
+    required: true
+  },
+  end: {
+    type: Date
+  }
+});
+
+//  creating compund index
+followersSchema.index({
+  followee: 1,
+  follower: 1
+}, { unique: true });
+
+export default mongoose.model('Followers', followersSchema);
--- app/models/QuestionFollower.js
+++ app/models/QuestionFollower.js
@@ -3,18 +3,26 @@
 import mongoose from  'mongoose';
 
 const questionFollowersSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Users',
-    required: true,
-    unique: true
-  },
   questionId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Questions',
     required:true,
     unique: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Users',
+    required: true
+  },
+  followedOn: {
+    type: Date,
+    required: true
   }
 });
+
+questionFollowersSchema.index({
+  questionId: 1,
+  userId: 1
+}, { unique: true });
 
 export default mongoose.model('QuestionFollowers', questionFollowersSchema);
