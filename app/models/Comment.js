71f23a176c7c580e411b5012cd2b94cf59cb6b6e
Ashok Dey <ashokdey100@gmail.com>
2017-08-14 17:54:00 +0530

    added Comment model

---
+++ app/models/Comment.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import mongoose from 'mongoose';
+
+const commentSchema = new mongoose.Schema({
+  body: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  commentedOn: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Answers'
+  },
+  commentedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Users'
+  }
+});
+
+export default mongoose.model('Comments', commentSchema);
