568d67f7369c9d3c32c44c0d31ef0c4f7036c537
Ashok Dey <ashokdey100@gmail.com>
2017-08-14 14:01:27 +0530

    functions for connection events of MongoDB

--- app/db/db.js
+++ app/db/db.js
@@ -11,11 +11,32 @@ import  mongoose from 'mongoose';
 // set the mongoose promise to use the default ES6 promise 
 mongoose.Promise = global.Promise;
 
-// connect to thedb using mongoose and setpool option to 10 
+// connect to the db using mongoose and setpool option to 10 
 mongoose.connect(process.env.MONGODB_URI, { 
   server: {
     poolSize: 10
   }
+});
+
+// console log on connection to DB
+mongoose.connection.on('connected', () => console.log('Connected to Database'));
+
+// console log the error while connecting to the DB
+mongoose.connection.on('error', (error) => console.log('**ERROR in connecting to db. Error: ', error));
+
+// on disconnect from the DB, console log a message
+mongoose.connection.on('disconnected', () => console.log('Disconnected from database'));
+
+/**
+ * on SIGINT which is actuallt POSIX fro app termination,
+ * close all the DB connection and exit the process with a negetive value.
+ * Also give a console log for the same
+ */
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('**Closing DB connection due to SIGINT');
+    process.exit(-1);
+  })
 });
 
 export default mongoose;
